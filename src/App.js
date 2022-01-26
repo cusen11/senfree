faeb4da12b84b7b43c79c05495108c2b825a6d5c
cusen11 <cusenbonchen@gmail.com>
2022-01-26 21:34:59 +0700

    sd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.png';
 import './App.css';
-
-function App() {
+import 'animate.css'; 
+import WOW from 'wowjs';
+import { useEffect } from 'react';
+function App() { 
+  useEffect(()=>{
+    new WOW.WOW({
+      live: false
+    }).init();
+  })
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="wow animate__backInRight animation" data-wow-duration="2s"><img src={logo} className="App-logo" alt="logo" /></div>
         <p>
           Nguyễn Thành Tuân
         </p> 
