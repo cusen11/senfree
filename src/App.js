4b00b1c6cbed84b3cb38672a284828145381cfc8
cusen11 <cusenbonchen@gmail.com>
2022-02-08 23:16:45 +0700

    ád

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import 'animate.css'; 
 import WOW from 'wowjs';
 import { useEffect } from 'react';
-import { AiFillSkype } from "react-icons/ai";
-import { AiFillFacebook } from "react-icons/ai";
+import { AiOutlineSkype } from "react-icons/ai";
+import { AiOutlineFacebook } from "react-icons/ai";
 import { AiOutlineMail } from "react-icons/ai";
+import { AiOutlinePhone } from "react-icons/ai";
 
 function App() { 
   useEffect(()=>{
@@ -18,9 +19,10 @@ function App() {
       <header className="App-header">
         <div className="wow animate__backInRight animation" data-wow-duration="2s"><img src={logo} className="App-logo" alt="logo" /></div>
         <ul style={{textAlight:'center'}}>
-          <li><a href='https://facebook.com/cusenbonchen' target={'_blank'} rel="noopener noreferrer"><AiFillFacebook/></a></li>
-          <li><a href='https://join.skype.com/invite/bIEMDrutWpit' target={'_blank'} rel="noopener noreferrer"><AiFillSkype/></a></li> 
+          <li><a href='https://facebook.com/cusenbonchen' target={'_blank'} rel="noopener noreferrer"><AiOutlineFacebook/></a></li>
+          <li><a href='https://join.skype.com/invite/bIEMDrutWpit' target={'_blank'} rel="noopener noreferrer"><AiOutlineSkype/></a></li> 
           <li><a href='mailto:cusenbonchen@gmail.com' target={'_blank'} rel="noopener noreferrer"><AiOutlineMail/></a></li> 
+        <li><a href='tel:0838008448' target={'_blank'} rel="noopener noreferrer"><AiOutlinePhone/></a></li>
         </ul>
         <p>
           Nguyễn Thành Tuân
